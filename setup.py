dcfd300f27c50b0a9a6be4c575d3c55f15af8494
Mike Smith <m.t.smith@sheffield.ac.uk>
2024-07-01 08:35:43 +0100

    rename

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'inference3d',
-  packages = ['inference3d'],
+  name = 'pathinference',
+  packages = ['pathinference'],
   version = '0.0.1',
   description = 'Infers 3d path from observation vectors at different times.',
   author = 'Mike Smith',
   author_email = 'm.t.smith@sheffield.ac.uk',
-  url = 'https://github.com/SheffieldMLtracking/inference3d.git',
-  download_url = 'https://github.com/SheffieldMLtracking/inference3d.git',
+  url = 'https://github.com/SheffieldMLtracking/pathinference.git',
+  download_url = 'https://github.com/SheffieldMLtracking/pathinference.git',
   keywords = ['flight','bayesian','inference','3d','position','orientation'],
   classifiers = [],
   install_requires=['numpy']
